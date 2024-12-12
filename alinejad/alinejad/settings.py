a39ea841e81ede70513f402e9aea82ab0ea1fe88
overdoseparsa <parsakhakiy@gmail.com>
2024-12-12 04:16:57 -0500

    adding app from this

--- alinejad/alinejad/settings.py
+++ alinejad/alinejad/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Base.app.BaseConfig' , # addding Base app 
 ]
 
 MIDDLEWARE = [
